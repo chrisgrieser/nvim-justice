49b2b9bb8a01c5f934a7065595308aae97aabace
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-18 15:51:56 +0100

    init: bootstrap

--- lua/justice/init.lua
+++ lua/justice/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 --------------------------------------------------------------------------------
 
----@param userConfig? PLACEHOLDER_plugin_name_short_capitalized.config
-M.setup = function(userConfig) require("PLACEHOLDER_plugin_name_short.config").setup(userConfig) end
+---@param userConfig? Justice.config
+M.setup = function(userConfig) require("justice.config").setup(userConfig) end
 
 --------------------------------------------------------------------------------
 return M
